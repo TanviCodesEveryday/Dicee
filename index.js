6f59e09db4a8cdb179c2fe64aa3afaf8095f9c2c
Tanvi Codes Everyday <145559687+TanviCodesEveryday@users.noreply.github.com>
2024-09-28 20:05:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var randomNumber1 = Math.floor(Math.random()*6);
+var randomNumber2 = Math.floor(Math.random()*6);
+var images = ["images/dice1.png","images/dice2.png","images/dice3.png","images/dice4.png","images/dice5.png","images/dice6.png"];
+var images = [
+    "./images/dice1.png",
+    "./images/dice2.png",
+    "./images/dice3.png",
+    "./images/dice4.png",
+    "./images/dice5.png",
+    "./images/dice6.png"
+];
+document.querySelector("img.img1").setAttribute("src", images[randomNumber1]);
+document.querySelector("img.img2").setAttribute("src", images[randomNumber2]);
+
+if(randomNumber1===randomNumber2){
+    document.querySelector("h1").textContent = "Draw!";
+}
+else if(randomNumber1>randomNumber2){
+    document.querySelector("h1").textContent = "Player 1 won";
+}
+else{
+    document.querySelector("h1").textContent = "Player 2 won";
+}
